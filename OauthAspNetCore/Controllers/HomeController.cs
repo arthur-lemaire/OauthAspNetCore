8299a348dc2ede16d83e6b05e9645d82acfdd96a
Arthur L <31710356+arthur-lemaire@users.noreply.github.com>
2024-01-27 19:18:48 +0100

    Ajoutez des fichiers projet.

---
+++ OauthAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OauthAspNetCore.Controllers
+{
+    [Route("api/home")]
+    public class HomeController : Controller
+    {
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            var responseData = new
+            {
+                message = "This endpoint is public.",
+                status = "success"
+            };
+
+            return Ok(responseData);
+        }
+
+        [Authorize]
+        [HttpGet("protected")]
+        public IActionResult Protected()
+        {
+            var responseData = new
+            {
+                message = "This endpoint is protected.",
+                status = "success"
+            };
+
+            return Ok(responseData);
+        }
+    }
+}
---
+++ OauthAspNetCore/Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    // Spécifie le schéma d'authentification par défaut utilisé pour les opérations d'authentification.
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    // Authority indique l'URL de base qui représente l'émetteur du jeton (Firebase dans ce cas).
+    // Utilisez l'ID du projet Firebase comme partie de l'URL.
+    options.Authority = "https://securetoken.google.com/PROJET-ID";
+
+    // TokenValidationParameters spécifie les paramètres pour valider les jetons d'authentification.
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // ValidateIssuer indique si l'émetteur du jeton doit être validé.
+        ValidateIssuer = true,
+        // Spécifie l'ID de l'émetteur à valider. Utilisez l'URL avec l'ID du projet Firebase.
+        ValidIssuer = "https://securetoken.google.com/PROJET-ID",
+
+        // ValidateAudience indique si l'audience du jeton doit être validée.
+        ValidateAudience = true,
+        // Spécifie l'ID de l'audience à valider. Utilisez l'ID du projet Firebase.
+        ValidAudience = "PROJET-ID",
+
+        // ValidateLifetime indique si la période de validité du jeton doit être validée.
+        ValidateLifetime = true
+    };
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAngularDev");
+
+// ... autres configurations ...
+
+app.UseRouting();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
